b7269232b608a2cd483d3b9db3f503a4017cd925
facusteckler86 <facundomsteckler@gmail.com>
2023-08-13 00:38:08 -0300

    noveno commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,3 +8,20 @@ import { Component } from '@angular/core';
 export class DashboardComponent {
 
 }
+
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+/**
+ * @title Card with multiple sections
+ */
+@Component({
+  selector: 'card-fancy-example',
+  templateUrl: 'card-fancy-example.html',
+  styleUrls: ['card-fancy-example.css'],
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
+})
+export class CardFancyExample {}
+
